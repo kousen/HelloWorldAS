e02d628b34a0542c249b05fcd62c76abf33d6896
Kenneth Kousen <ken.kousen@kousenit.com>
2014-09-12 23:25:07 -0400

    change display text size

--- app/src/main/java/com/nfjs/helloworldas/MyActivity.java
+++ app/src/main/java/com/nfjs/helloworldas/MyActivity.java
@@ -2,6 +2,7 @@ package com.nfjs.helloworldas;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
