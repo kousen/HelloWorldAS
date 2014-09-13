f1ba2623a4a8e3baef1a9ee544b3e58a2336232d
Kenneth Kousen <ken.kousen@kousenit.com>
2014-09-12 23:27:32 -0400

    Log activity events

--- app/src/main/java/com/nfjs/helloworldas/MyActivity.java
+++ app/src/main/java/com/nfjs/helloworldas/MyActivity.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 
 
 public class MyActivity extends Activity {
+    private static final String TAG = "MyActivity";
+
     private TextView textView;
     private EditText editText;
 
@@ -19,6 +21,8 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        Log.d(TAG, "onCreate");
 
         textView = (TextView) findViewById(R.id.text_view);
         editText = (EditText) findViewById(R.id.edit_text);
@@ -34,6 +38,42 @@ public class MyActivity extends Activity {
     public void sayHello(View view) {
         String name = editText.getText().toString();
         textView.setText(String.format("Hello, %1$s!", name));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
     }
 
     @Override
