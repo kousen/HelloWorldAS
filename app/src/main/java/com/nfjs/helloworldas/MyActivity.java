b294c78516bfc1dd523603d4658b4ccde041d732
Kenneth Kousen <ken.kousen@kousenit.com>
2014-09-12 23:14:20 -0400

    Initial commit

---
+++ app/src/main/java/com/nfjs/helloworldas/MyActivity.java
@@ -0,0 +1,60 @@
+package com.nfjs.helloworldas;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MyActivity extends Activity {
+    private TextView textView;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        textView = (TextView) findViewById(R.id.text_view);
+        editText = (EditText) findViewById(R.id.edit_text);
+        Button helloButton = (Button) findViewById(R.id.hello_button);
+        helloButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sayHello(v);
+            }
+        });
+    }
+
+    public void sayHello(View view) {
+        String name = editText.getText().toString();
+        textView.setText(String.format("Hello, %1$s!", name));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
