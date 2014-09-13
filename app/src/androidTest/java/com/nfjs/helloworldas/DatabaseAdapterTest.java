9da65d849501d3b80c57e621ba93c43a9751d68e
Kenneth Kousen <ken.kousen@kousenit.com>
2014-09-13 01:51:43 -0400

    test database adapter

---
+++ app/src/androidTest/java/com/nfjs/helloworldas/DatabaseAdapterTest.java
@@ -0,0 +1,33 @@
+package com.nfjs.helloworldas;
+
+import android.test.AndroidTestCase;
+
+import java.util.List;
+
+public class DatabaseAdapterTest extends AndroidTestCase {
+    private DatabaseAdapter dba;
+
+    @Override
+    protected void setUp() throws Exception {
+        dba = new DatabaseAdapter(getContext());
+        dba.open();
+    }
+
+    public void testInsertAndGetAllNames() {
+        dba.insertName("Fred");
+        dba.insertName("Barney");
+        List<String> names = dba.getAllNames();
+        assertTrue(names.contains("Fred"));
+        assertTrue(names.contains("Barney"));
+        dba.deleteAllNames();
+    }
+
+    public void testDeleteName() {
+        int count = dba.getAllNames().size();
+        dba.insertName("Fred");
+        assertEquals(count + 1, dba.getAllNames().size());
+
+        dba.deleteName("Fred");
+        assertEquals(count, dba.getAllNames().size());
+    }
+}
