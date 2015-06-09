f836cc15e3a81954d896124ffd2d1b9e045ddc24
Kenneth Kousen <ken.kousen@kousenit.com>
2015-06-09 15:02:17 -0700

    Externalize strings

--- app/src/main/java/com/nfjs/helloworldas/NameFragment.java
+++ app/src/main/java/com/nfjs/helloworldas/NameFragment.java
@@ -11,11 +11,11 @@ public class NameFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("This is an alert")
+        builder.setTitle(R.string.alert_title)
                 .setIcon(android.R.drawable.star_on)
                 .setMessage(String.format("You clicked on %s",
                         getArguments().getString("name")))
-                .setNeutralButton("Yeah, I know", null);
+                .setNeutralButton(R.string.alert_button_label, null);
         return builder.create();
     }
 }
