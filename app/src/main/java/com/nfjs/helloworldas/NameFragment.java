aaf09c18a091671a92dc99ccc6f81c20514f9616
Kenneth Kousen <ken.kousen@kousenit.com>
2015-06-10 00:05:10 -0700

    Rate the names

--- app/src/main/java/com/nfjs/helloworldas/NameFragment.java
+++ app/src/main/java/com/nfjs/helloworldas/NameFragment.java
@@ -1,6 +1,7 @@
 package com.nfjs.helloworldas;
 
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
@@ -10,21 +11,44 @@ import android.widget.Toast;
 
 public class NameFragment extends DialogFragment {
 
+    public static interface Rateable {
+        void modifyRating(String name, int amount);
+    }
+
+    private Rateable rater;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        rater = (Rateable) activity;
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final String name = getArguments().getString("name");
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.alert_title)
                 .setIcon(android.R.drawable.star_on)
-                .setMessage(String.format("You clicked on %s",
-                        getArguments().getString("name")))
-                .setNeutralButton(R.string.alert_button_label,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Toast.makeText(getActivity(), R.string.toastFromNeutralButton,
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        });
+                .setMessage(String.format("How do you feel about %s?", name))
+                .setPositiveButton("Positive, +1", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        rater.modifyRating(name, 1);
+                    }
+                })
+                .setNeutralButton("Neutral, 0", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), name + " is going to be disappointed",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Negative, -1", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        rater.modifyRating(name, -1);
+                    }
+                });
         return builder.create();
     }
 }
--- app/src/main/java/com/nfjs/helloworldas/WelcomeActivity.java
+++ app/src/main/java/com/nfjs/helloworldas/WelcomeActivity.java
@@ -11,13 +11,17 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
-public class WelcomeActivity extends Activity {
+public class WelcomeActivity extends Activity implements NameFragment.Rateable {
     private TextView greetingText;
     private DatabaseAdapter adapter;
+    private Map<String, Integer> ratings = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +72,17 @@ public class WelcomeActivity extends Activity {
     }
 
     @Override
+    public void modifyRating(String name, int amount) {
+        if (ratings.get(name) != null) {
+            ratings.put(name, ratings.get(name) + amount);
+        } else {
+            ratings.put(name, amount);
+        }
+        Toast.makeText(this, String.format("%s has rating %d", name, ratings.get(name)),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         adapter.close();
@@ -78,6 +93,12 @@ public class WelcomeActivity extends Activity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString("display", greetingText.getText().toString());
+
+        String[] names = ratings.keySet().toArray(new String[ratings.keySet().size()]);
+        outState.putStringArray("names", names);
+        for (String name : names) {
+            outState.putInt(name, ratings.get(name));
+        }
     }
 
     @SuppressWarnings("NullableProblems")
@@ -85,6 +106,10 @@ public class WelcomeActivity extends Activity {
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         greetingText.setText(savedInstanceState.getString("display"));
+        String[] names = savedInstanceState.getStringArray("names");
+        for (String name : names) {
+            ratings.put(name, savedInstanceState.getInt(name));
+        }
     }
 
     @Override
