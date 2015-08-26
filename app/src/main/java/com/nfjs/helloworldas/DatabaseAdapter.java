0c604634620f216127572084a03f678871327cf8
Kenneth Kousen <ken.kousen@kousenit.com>
2015-08-26 14:48:55 -0400

    Close DB adapter

--- app/src/main/java/com/nfjs/helloworldas/DatabaseAdapter.java
+++ app/src/main/java/com/nfjs/helloworldas/DatabaseAdapter.java
@@ -47,7 +47,9 @@ public class DatabaseAdapter {
         Cursor cursor = database.rawQuery(
                 "select name from names where name=?",
                 new String[]{ name });
-        return cursor.getCount() >= 1;
+        boolean yes = cursor.getCount() >= 1;
+        cursor.close();
+        return yes;
     }
 
     public long insertName(String name) {
