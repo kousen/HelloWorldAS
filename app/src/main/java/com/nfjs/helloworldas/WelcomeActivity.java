acf23362d3ac38398ab3c88edf21fcef0ff89d04
Kenneth Kousen <ken.kousen@kousenit.com>
2015-12-02 14:35:19 -0500

    Updating gradle version

---
+++ app/src/main/java/com/nfjs/helloworldas/WelcomeActivity.java
@@ -0,0 +1,90 @@
+package com.nfjs.helloworldas;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class WelcomeActivity extends Activity {
+    private TextView greetingText;
+    private DatabaseAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        if (getActionBar() != null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        String name = getIntent().getStringExtra("name");
+        greetingText = (TextView) findViewById(R.id.greeting_text);
+        String format = getString(R.string.greeting);
+        greetingText.setText(String.format(format, name));
+
+        adapter = new DatabaseAdapter(this);
+        adapter.open();
+        if (!adapter.exists(name)) {
+            adapter.insertName(name);
+        }
+
+        List<String> names = adapter.getAllNames();
+        ListView listView = (ListView) findViewById(R.id.list_view);
+
+        ArrayAdapter<String> arrayAdapter
+                = new ArrayAdapter<String>(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    names);
+
+        listView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        adapter.close();
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("display", greetingText.getText().toString());
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        greetingText.setText(savedInstanceState.getString("display"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.welcome, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
