d41dee8ddc48ee0e062546eab5f188779648ba6a
Kenneth Kousen <ken.kousen@kousenit.com>
2017-12-08 10:45:44 -0500

    Fix query method in db adapter

--- app/src/main/java/com/nfjs/helloworldas/DatabaseAdapter.java
+++ app/src/main/java/com/nfjs/helloworldas/DatabaseAdapter.java
@@ -28,11 +28,12 @@ public class DatabaseAdapter {
     private Cursor getAllEntries() {
         String[] columns = new String[1];
         columns[0] = "name";
-        return database.query("names", columns, null, null, null, null, null);
+        return database.query("names", columns, null, null,
+                null, null, null);
     }
 
     public List<String> getAllNames() {
-        ArrayList<String> names = new ArrayList<String>();
+        ArrayList<String> names = new ArrayList<>();
         Cursor cursor = getAllEntries();
         if (cursor.moveToFirst()) {
             do {
@@ -47,7 +48,9 @@ public class DatabaseAdapter {
         Cursor cursor = database.rawQuery(
                 "select name from names where name=?",
                 new String[]{ name });
-        return cursor.getCount() >= 1;
+        boolean result = cursor.getCount() >= 1;
+        cursor.close();
+        return result;
     }
 
     public long insertName(String name) {
