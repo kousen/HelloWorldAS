babc625b78d9be87a35760659721c7be74723b9e
Ken Kousen <ken.kousen@kousenit.com>
2018-03-06 01:29:09 -0500

    Clean up imports

--- app/src/androidTest/java/com/nfjs/helloworldas/UserRoomTest.java
+++ app/src/androidTest/java/com/nfjs/helloworldas/UserRoomTest.java
@@ -4,14 +4,12 @@ import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
